29314570317553a9bb1206a897d7e12046c75562
dioscar_cleverdesign <Dioscar@cleverdesign.com>
2015-12-28 08:37:56 -0500

    init

---
+++ Gala_MVC_Project/Gala_MVC_Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Gala_MVC_Project.Startup))]
+namespace Gala_MVC_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
