320b4a814fc42b604b3e037ac7857e706ac0bbd0
Lewis Lin <linnobita@gmail.com>
2015-12-28 16:22:05 -0500

    Added Home page, updated css, images, fonts

--- Gala_MVC_Project/Gala_MVC_Project/Controllers/HomeController.cs
+++ Gala_MVC_Project/Gala_MVC_Project/Controllers/HomeController.cs
@@ -13,18 +13,5 @@ namespace Gala_MVC_Project.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
