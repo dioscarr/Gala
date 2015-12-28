ba762fdcdf2abe5115f8e4a93188b4ac6ba014b7
dioscar_cleverdesign <Dioscar@cleverdesign.com>
2015-12-28 15:37:38 -0500

    more changes

---
+++ Gala_MVC_Project/Gala_MVC_Project/Areas/Admin/Controllers/AdminAboutUSController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Gala_MVC_Project.Areas.Admin.Controllers
+{
+    public class AdminAboutUSController : Controller
+    {
+        // GET: Admin/AdminAboutUS
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Gala_MVC_Project/Gala_MVC_Project/Areas/Admin/Models/CountryModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DAL.Models;
+using BLL;
+
+namespace Gala_MVC_Project.Areas.Admin.Models
+{
+    public class CountryModel
+    {
+        public int MyProperty { get; set; }
+
+
+
+        public CountryModel()
+        { 
+        
+        
+        }
+    }
+}
