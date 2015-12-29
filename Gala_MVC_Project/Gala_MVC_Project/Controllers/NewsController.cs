e36c57df639babc21d31cea68c742e72f9f3fb10
Lewis Lin <linnobita@gmail.com>
2015-12-29 17:17:14 -0500

    News and Events pages

---
+++ Gala_MVC_Project/Gala_MVC_Project/Controllers/NewsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Gala_MVC_Project.Controllers
+{
+    public class NewsController : Controller
+    {
+        // GET: News
+        public ActionResult NewList()
+        {
+            return View();
+        }
+        public ActionResult NewArchive()
+        {
+            return View();
+        }
+    }
+}
