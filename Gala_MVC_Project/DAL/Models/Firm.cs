b87c291802a7d20cb125bc620497b97aff16a271
Lewis Lin <linnobita@gmail.com>
2016-01-20 14:41:55 -0500

    FCRelation

--- Gala_MVC_Project/DAL/Models/Firm.cs
+++ Gala_MVC_Project/DAL/Models/Firm.cs
@@ -19,6 +19,7 @@ namespace DAL.Models
         {
             this.CMFRelation = new HashSet<CMFRelation>();
             this.Events1 = new HashSet<Events>();
+            this.FCRelation = new HashSet<FCRelation>();
             this.Team = new HashSet<Team>();
         }
     
@@ -45,6 +46,8 @@ namespace DAL.Models
         public virtual ICollection<CMFRelation> CMFRelation { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Events> Events1 { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<FCRelation> FCRelation { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Team> Team { get; set; }
     }
