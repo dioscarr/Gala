b317978d3390ef73fc1994a9dd9024db1794e9cd
Lewis Lin <linnobita@gmail.com>
2016-02-29 17:13:04 -0500

    Membership

---
+++ Gala_MVC_Project/GalaLaw/Controllers/MembershipAssociateController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GalaLaw.Controllers
+{
+    public class MembershipAssociateController : Controller
+    {
+        // GET: MembershipAssociate
+        public ActionResult MembershipAssociate()
+        {
+            return View();
+        }
+        public ActionResult WelcomeMembershipAssociate()
+        {
+            return View();
+        }
+    }
+}
